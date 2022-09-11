355fff1f962f7feaca03607e475bf3f65f8047e4
uyeah0 <uyeah029@gmail.com>
2022-09-11 23:32:00 +0900

    #10 인터페이스 응용 실습

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -75,9 +76,13 @@ namespace CSClass2
             (basic as TestClass).someting();
 
             Child c = new Child();
+            
             Parent childAsParent = c;
             IDisposable childAsDispoable = c;
             IComparable<Child> childAsComparable = c;
+
+            File.WriteAllText(@"c:\TEMP\test.txt", "문자열 메시지를 씁니다");
+            Console.WriteLine(File.ReadAllText(@"c:\TEMP\test.txt"));
         }
 
         class TestClass : IBasic
